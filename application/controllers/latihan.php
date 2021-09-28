15d28022e910b7810aacc151c5f7ad9ca77915d3
DesfiNurHanafi <desfihanafi@gmail.com>
2021-09-28 12:17:49 +0700

    .

---
+++ application/controllers/latihan.php
@@ -0,0 +1,12 @@
+<?php
+class latihan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Desfi Nur Hanafi
+        Saya tinggal di daerah Tangerang
+        Olahraga yang saya sukai adalah
+        Badminton";
+    }
+}
